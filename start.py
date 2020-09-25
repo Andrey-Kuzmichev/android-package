3129e7aa5b97abc2a2d7e16731b6cc2ab950d186
Андрей <40794680+Andrey-Kuzmichev@users.noreply.github.com>
2020-09-26 01:04:03 +0300

    add

---
+++ start.py
@@ -0,0 +1,24 @@
+import subprocess
+
+
+def start():
+    with open('package.list', 'r') as file:
+        for line in file:
+            package = str(line).split(':')[1].strip()
+            if str(line).startswith('-package'):
+                cmd = 'adb.exe shell pm uninstall -k --user 0  {}'.format(package)
+                out_data = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+                print('[Uninstalled] {}: {}'.format(package, out_data))
+            elif str(line).startswith('=package'):
+                cmd = 'adb.exe shell pm disable-user --user 0 {}'.format(package)
+                out_data = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+                print('[Disabled] {}: {}'.format(package, out_data))
+            elif str(line).startswith('+package'):
+                cmd = 'adb.exe shell cmd package install-existing {}'.format(package)
+                out_data = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.read().decode()
+                print('[Installed] {}: {}'.format(package, out_data))
+        file.close()
+
+
+if __name__ == '__main__':
+    start()
